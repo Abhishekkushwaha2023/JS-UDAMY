67131540c3d427fa655425359ffb780cd1b34e25
Abhishekkushwaha2023 <abhikushwaha20022004@gmail.com>
2023-12-29 15:33:41 +0530

    first commit

---
+++ Assignments of fundament 1/assign1/assign1.js
@@ -0,0 +1,9 @@
+let continent = "Asia";
+let country = "India";
+let population = "140 Crores";
+
+console.log(continent);
+console.log(country);
+console.log(population);
+
+
---
+++ Assignments of fundament 1/assign2/assign2.js
@@ -0,0 +1,13 @@
+let isIslandAreThere = true;
+console.log(typeof isIslandAreThere);
+
+let population = 125000;
+console.log(typeof population);
+
+let country = "India";
+console.log(typeof country);
+
+let language;
+console.log(typeof language);
+
+
