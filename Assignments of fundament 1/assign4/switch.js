915f3dca4de6907f6b8e360a30f5cadebf2dd7d8
Abhishekkushwaha2023 <abhikushwaha20022004@gmail.com>
2024-02-28 18:40:10 +0530

    done by me

--- Assignments of fundament 1/assign4/switch.js
+++ Assignments of fundament 1/assign4/switch.js
@@ -1,25 +1,51 @@
-let day = "Tuesday";
+// const day = "Tuesday";
 
 
-switch (day) {
-    case "Monday":
-        console.log("Lets plan for the Week");
-        console.log("Today I am going to study Physic");
-        break;
-    case "Tuesday":
-        console.log("Today I am going to study Maths");
-        console.log("In the evening I am going to the temple");
-        break;
-    case "Wednesday":
-        console.log("Today I am going to study English");
-        break;
-    case "Thursday":
-    case "Friday":
-        console.log("Today I am going to study Gk");
-        break;
-    case "Saturday":
-    case "Sunday":
-        console.log("Today I am going to Give Test of the whole Week");
-        break;
+// switch (day) {
+//     case "Monday":
+//         console.log("Lets plan for the Week");
+//         console.log("Today I am going to study Physic");
+//         break;
+//     case "Tuesday":
+//         console.log("Today I am going to study Maths");
+//         console.log("In the evening I am going to the temple");
+//         break;
+//     case "Wednesday":
+//         console.log("Today I am going to study English");
+//         break;
+//     case "Thursday":
+//     case "Friday":
+//         console.log("Today I am going to study Gk");
+//         break;
+//     case "Saturday":
+//     case "Sunday":
+//         console.log("Today I am going to Give Test of the whole Week");
+//         break;
+// }
+
+
+
+
+// Done My self realy
+
+const day = "Saturday";
+
+if (day === "Monday") {
+    console.log("Lets plan for the Week.");
+    console.log("Today I am going to study Maths.");
+} else if (day === "Tuesday") {
+    console.log("Today I am going to study Physics.");
+    console.log("In the evening I am going to the temple.");
+} else if (day === "Wednesday") {
+    console.log("Today I am going to study English.");
+} else if (day === "Thursday" || day === "Friday") {
+    console.log("Today I am going to study Gk.");
+} else if (day === "Saturday" || day === "Sunday") {
+    console.log("Today I am going to Give Test of the whole Week.");
+} else {
+    console.log("Not a define day. ");
 }
+
+
+
 
